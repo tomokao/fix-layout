4a1c86b42e2a39183b2f761f000c9424de3a9aa9
Matheus Okamoto <128179754+tomokao@users.noreply.github.com>
2023-12-03 00:03:36 -0300

    Add clap and configuration parameters

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
 use std::process::Command;
 
+use clap::Parser;
 use fix_layout::backend::{x11::X11, Backend};
 use regex::Regex;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    regex: String,
+    #[arg(short, long)]
+    active_command: String,
+    #[arg(short, long)]
+    unactive_command: String,
+}
+
+fn command_from_string(s: &str) -> Command {
+    if cfg!(target_os = "windows") {
+        let mut command = Command::new("cmd");
+        command.arg("/C").arg(s);
+        command
+    } else {
+        let mut command = Command::new("sh");
+        command.arg("-c").arg(s);
+        command
+    }
+}
+
 fn main() {
+    let args = Args::parse();
+
     let mut backend = X11::create().unwrap();
-    let regex = Regex::new(r"arch").unwrap();
+    let regex = Regex::new(&args.regex).unwrap();
     loop {
         backend.wait_for_active_window();
-        _ = Command::new("setxkbmap")
-            .args([
-                "-model",
-                "abnt2",
-                "-layout",
-                "br",
-                "-variant",
-                if backend.active_window_matches(&regex) {
-                    "abnt2"
-                } else {
-                    "dvorak"
-                },
-            ])
-            .output();
+        _ = (if backend.active_window_matches(&regex) {
+            command_from_string(&args.active_command)
+        } else {
+            command_from_string(&args.unactive_command)
+        })
+        .output();
     }
 }
